ba387442d53588a9e30289a6210523da58906e38
xukun <xukun17@sina.com>
2024-07-27 12:57:25 +0800

    up to v0.0.4

--- module_t.go
+++ module_t.go
@@ -9,8 +9,8 @@ import (
 
 const (
 	theModuleName     = "github.com/starter-go/i18n"
-	theModuleVersion  = "v0.0.3"
-	theModuleRevision = 3
+	theModuleVersion  = "v0.0.4"
+	theModuleRevision = 4
 )
 
 ////////////////////////////////////////////////////////////////////////////////
